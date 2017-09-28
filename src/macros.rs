ed510910dd29081a1da5a977238a2577bd20e4e3
Kevin Yeh <kevinyeah@utexas.edu>
2017-09-28 14:11:36 -0400

    remove old macros

--- src/macros.rs
+++ src/macros.rs
@@ -240,38 +240,6 @@ macro_rules! bson {
     };
 }
 
-// /// Construct a BSON value
-// #[macro_export]
-// macro_rules! bson {
-//     ([]) => {{ $crate::Bson::Array(Vec::new()) }};
-
-//     ([$($val:tt),*]) => {{
-//         let mut array = Vec::new();
-
-//         $(
-//             array.push(bson!($val));
-//         )*
-
-//         $crate::Bson::Array(array)
-//     }};
-
-//     ([$val:expr]) => {{
-//         $crate::Bson::Array(vec!(::std::convert::From::from($val)))
-//     }};
-
-//     ({ $($k:expr => $v:tt),* }) => {{
-//         $crate::Bson::Document(doc! {
-//             $(
-//                 $k => $v
-//             ),*
-//         })
-//     }};
-
-//     ($val:expr) => {{
-//         ::std::convert::From::from($val)
-//     }};
-// }
-
 #[macro_export]
 macro_rules! doc {
     () => {{ $crate::Document::new() }};
@@ -281,20 +249,3 @@ macro_rules! doc {
         object
     }};
 }
-
-// /// Construct a BSON Document
-// #[macro_export]
-// macro_rules! doc {
-//     () => {{ $crate::Document::new() }};
-
-//     ( $($key:expr => $val:tt),* ) => {{
-        
-//         let mut document = $crate::Document::new();
-
-//         $(
-//             document.insert_bson($key.to_owned(), bson!($val));
-//         )*
-
-//         document
-//     }};
-// }
