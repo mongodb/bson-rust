e1bee2b9a4c3a14e774a842d39ee2c9a322de2b5
Saghm Rossi <saghmrossi@gmail.com>
2020-04-06 17:05:45 -0400

    minor: properly format and lint serde-tests

--- .evergreen/check-clippy.sh
+++ .evergreen/check-clippy.sh
@@ -4,3 +4,5 @@ set -o errexit
 
 . ~/.cargo/env
 cargo clippy --all-targets --all-features -p bson -- -D warnings
+
+cd serde-tests && cargo clippy --all-targets --all-features -p serde-tests -- -D warnings
