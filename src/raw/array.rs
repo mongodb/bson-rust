1769a145120ab877995de0579ada23b71c1b360b
Patrick Freed <patrick.freed@mongodb.com>
2021-10-06 15:23:22 -0400

    fix typo

--- src/raw/array.rs
+++ src/raw/array.rs
@@ -150,7 +150,7 @@ impl<'a> IntoIterator for &'a RawArray {
     }
 }
 
-/// An iterator over borrwed raw BSON array values.
+/// An iterator over borrowed raw BSON array values.
 pub struct RawArrayIter<'a> {
     inner: RawDocumentIter<'a>,
 }
