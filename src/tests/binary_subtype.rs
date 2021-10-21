77dcc739ebd9c8070e108638dba6ab6aa1aea3e1
Karmen Liang <karmen.liang@mongodb.com>
2021-10-21 13:43:00 -0700

    change test to use column subtype

--- src/tests/binary_subtype.rs
+++ src/tests/binary_subtype.rs
@@ -6,7 +6,7 @@ fn from_u8() {
     // Check the endpoints of the defined, reserved, and user-defined subtype ranges.
     assert_eq!(BinarySubtype::from(0x00), BinarySubtype::Generic);
     assert_eq!(BinarySubtype::from(0x06), BinarySubtype::Encrypted);
-    assert_eq!(BinarySubtype::from(0x07), BinarySubtype::Reserved(0x07));
+    assert_eq!(BinarySubtype::from(0x07), BinarySubtype::Column);
     assert_eq!(BinarySubtype::from(0x7F), BinarySubtype::Reserved(0x7F));
     assert_eq!(BinarySubtype::from(0x80), BinarySubtype::UserDefined(0x80));
     assert_eq!(BinarySubtype::from(0xFF), BinarySubtype::UserDefined(0xFF));
