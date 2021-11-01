eccb081bd6e8df1caa7031ee5a0f508a1960d742
Karmen Liang <karmen.liang@mongodb.com>
2021-11-01 13:54:44 -0700

    rename method calls

--- src/tests/datetime.rs
+++ src/tests/datetime.rs
@@ -5,7 +5,7 @@ fn rfc3339_to_datetime() {
     let rfc = "2020-06-09T10:58:07.095Z";
     let date = chrono::DateTime::<chrono::Utc>::from_str(rfc).unwrap();
     assert_eq!(
-        crate::DateTime::from_rfc3339(rfc).unwrap(),
+        crate::DateTime::parse_rfc3339_str(rfc).unwrap(),
         crate::DateTime::from_chrono(date)
     );
 }
@@ -15,7 +15,7 @@ fn invalid_rfc3339_to_datetime() {
     let a = "2020-06-09T10:58:07-095Z";
     let b = "2020-06-09T10:58:07.095";
     let c = "2020-06-09T10:62:07.095Z";
-    assert!(crate::DateTime::from_rfc3339(a).is_err());
-    assert!(crate::DateTime::from_rfc3339(b).is_err());
-    assert!(crate::DateTime::from_rfc3339(c).is_err());
+    assert!(crate::DateTime::parse_rfc3339_str(a).is_err());
+    assert!(crate::DateTime::parse_rfc3339_str(b).is_err());
+    assert!(crate::DateTime::parse_rfc3339_str(c).is_err());
 }
