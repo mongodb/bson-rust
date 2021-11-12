5ca2de3d188cf7425978c32fd537c7d844436d6e
Patrick Freed <patrick.freed@mongodb.com>
2021-11-12 13:25:25 -0500

    minor: hide work-in-progress raw BSON API

--- src/lib.rs
+++ src/lib.rs
@@ -272,20 +272,19 @@ pub use self::{
     bson::{Array, Binary, Bson, DbPointer, Document, JavaScriptCodeWithScope, Regex, Timestamp},
     datetime::DateTime,
     de::{
-        from_bson, from_bson_with_options, from_document, from_document_with_options, from_reader, from_reader_utf8_lossy,
-        from_slice, from_slice_utf8_lossy, Deserializer, DeserializerOptions,
+        from_bson, from_bson_with_options, from_document, from_document_with_options, from_reader,
+        from_reader_utf8_lossy, from_slice, from_slice_utf8_lossy, Deserializer,
+        DeserializerOptions,
     },
     decimal128::Decimal128,
-    raw::{
-        RawArray, RawBinary, RawBson, RawDbPointer, RawDocument, RawDocumentBuf, RawJavaScriptCodeWithScope,
-        RawRegex,
-    },
     ser::{
         to_bson, to_bson_with_options, to_document, to_document_with_options, to_vec, Serializer,
         SerializerOptions,
     },
     uuid::{Uuid, UuidRepresentation},
 };
+
+pub(crate) use self::raw::RawDocument;
 
 #[macro_use]
 mod macros;
@@ -296,7 +295,7 @@ pub mod decimal128;
 pub mod document;
 pub mod extjson;
 pub mod oid;
-pub mod raw;
+pub(crate) mod raw;
 pub mod ser;
 pub mod serde_helpers;
 pub mod spec;
