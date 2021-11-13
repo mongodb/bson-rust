3f34f8c0b71da858814965ffe56647001c894dca
bbrunell <bbrunell@student.42.fr>
2021-11-13 11:34:44 +0100

    Fix: Array relaxed extended json

--- src/bson.rs
+++ src/bson.rs
@@ -379,7 +379,7 @@ impl Bson {
             }
             Bson::Double(v) => json!(v),
             Bson::String(v) => json!(v),
-            Bson::Array(v) => json!(v),
+            Bson::Array(v) => Value::Array(v.into_iter().map(Bson::into_relaxed_extjson).collect()),
             Bson::Document(v) => {
                 Value::Object(v.into_iter().map(|(k, v)| (k, Value::from(v))).collect())
             }
