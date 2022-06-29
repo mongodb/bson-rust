a6eb9039bbca2cc86cf3d506043c4fec1b686213
Abraham Egnor <abraham.egnor@mongodb.com>
2022-06-29 10:43:08 -0400

    RUST-1374 Pin all dependencies when checking MSRV compilation

--- .evergreen/compile-only.sh
+++ .evergreen/compile-only.sh
@@ -5,4 +5,9 @@ set -o errexit
 . ~/.cargo/env
 rustup update $RUST_VERSION
 
+# pin all dependencies when checking msrv compilation
+if [  "$MSRV" = "true" ]; then
+    cp .evergreen/Cargo.lock.msrv Cargo.lock
+fi
+
 rustup run $RUST_VERSION cargo build
