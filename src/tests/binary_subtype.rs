230dab13f5d57ae81be81a77a2c7ac7cc703c78a
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2024-01-23 15:32:48 -0600

    RUST-1716 Add BSON Binary Data subtype Sensitive (#454)
    - Sync BSON corpus test
    - Implement and test BinarySubtype::Sensitive which maps to 0x08

--- src/tests/binary_subtype.rs
+++ src/tests/binary_subtype.rs
@@ -7,6 +7,7 @@ fn from_u8() {
     assert_eq!(BinarySubtype::from(0x00), BinarySubtype::Generic);
     assert_eq!(BinarySubtype::from(0x06), BinarySubtype::Encrypted);
     assert_eq!(BinarySubtype::from(0x07), BinarySubtype::Column);
+    assert_eq!(BinarySubtype::from(0x08), BinarySubtype::Sensitive);
     assert_eq!(BinarySubtype::from(0x7F), BinarySubtype::Reserved(0x7F));
     assert_eq!(BinarySubtype::from(0x80), BinarySubtype::UserDefined(0x80));
     assert_eq!(BinarySubtype::from(0xFF), BinarySubtype::UserDefined(0xFF));
