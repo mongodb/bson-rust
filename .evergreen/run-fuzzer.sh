9396af9aa95d6bdb0c082d64cff9f36e01c8729e
Patrick Meredith <pmeredit@protonmail.com>
2024-12-11 09:48:52 -0500

    Actually, Devin did not modify the sh script :(

--- .evergreen/run-fuzzer.sh
+++ .evergreen/run-fuzzer.sh
@@ -10,3 +10,7 @@ cd fuzz
 cargo +nightly fuzz run deserialize -- -rss_limit_mb=4096 -max_total_time=60
 cargo +nightly fuzz run raw_deserialize -- -rss_limit_mb=4096 -max_total_time=60
 cargo +nightly fuzz run iterate -- -rss_limit_mb=4096 -max_total_time=60
+cargo +nightly fuzz run malformed_length -- -rss_limit_mb=4096 -max_total_time=60
+cargo +nightly fuzz run type_markers -- -rss_limit_mb=4096 -max_total_time=120
+cargo +nightly fuzz run string_handling -- -rss_limit_mb=4096 -max_total_time=120
+cargo +nightly fuzz run serialization -- -rss_limit_mb=4096 -max_total_time=60
