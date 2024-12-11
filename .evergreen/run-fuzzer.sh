7c60a2277a8683fc5d5ca9bb87188ca6266b6d93
Patrick Meredith <pmeredit@protonmail.com>
2024-12-11 10:54:29 -0500

    Devin is still trying to run things too, long, manual update

--- .evergreen/run-fuzzer.sh
+++ .evergreen/run-fuzzer.sh
@@ -12,25 +12,26 @@ mkdir -p artifacts
 # Function to run fuzzer and collect crashes
 run_fuzzer() {
     target=$1
+    time=$2
     echo "Running fuzzer for $target"
     # Run fuzzer and redirect crashes to artifacts directory
     RUST_BACKTRACE=1 cargo +nightly fuzz run $target -- \
         -rss_limit_mb=4096 \
-        -max_total_time=3600 \
+        -max_total_time=$time \
         -artifact_prefix=artifacts/ \
         -print_final_stats=1
 }
 
 # Run existing targets
-run_fuzzer "deserialize"
-run_fuzzer "raw_deserialize"
-run_fuzzer "iterate"
+run_fuzzer "deserialize" 60
+run_fuzzer "raw_deserialize" 60
+run_fuzzer "iterate" 60
 
 # Run new security-focused targets
-run_fuzzer "malformed_length"
-run_fuzzer "type_markers"
-run_fuzzer "string_handling"
-run_fuzzer "serialization"
+run_fuzzer "malformed_length" 60
+run_fuzzer "type_markers" 120
+run_fuzzer "string_handling" 120
+run_fuzzer "serialization" 120
 
 # If any crashes were found, save them as test artifacts
 if [ "$(ls -A artifacts)" ]; then
