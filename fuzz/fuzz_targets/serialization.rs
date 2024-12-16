100f70e6ca2b3906f00ab1c434adf956e7c660c5
Patrick Meredith <pmeredit@protonmail.com>
2024-12-16 17:47:21 -0500

    Still need to worry about double nan

--- fuzz/fuzz_targets/serialization.rs
+++ fuzz/fuzz_targets/serialization.rs
@@ -1,9 +1,36 @@
 #![no_main]
 use bson::{
     raw::{RawDocument, RawDocumentBuf},
+    Bson,
     Document,
 };
 use libfuzzer_sys::fuzz_target;
+
+fn compare_docs(doc1: &Document, doc2: &Document) -> bool {
+    if doc1.len() != doc2.len() {
+        return false;
+    }
+    for (key, value) in doc1 {
+        if !doc2.contains_key(key) {
+            return false;
+        }
+        if let Some(val2) = doc2.get(key) {
+            match (value, val2) {
+                (Bson::Double(d1), Bson::Double(d2)) => {
+                    if (!d1.is_nan() || !d2.is_nan()) && d1 != d2 {
+                        return false;
+                    }
+                }
+                (v1, v2) => {
+                    if v1 != v2 {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    true
+}
 
 fuzz_target!(|input: &[u8]| {
     if let Ok(rawdoc) = RawDocument::from_bytes(&input) {
@@ -15,7 +42,13 @@ fuzz_target!(|input: &[u8]| {
                 let reserialized_doc = Document::try_from(reserialized).unwrap();
                 // Ensure that the reserialized document is the same as the original document, the
                 // bytes can differ while still resulting in the same Document.
-                assert_eq!(doc, reserialized_doc, "reserialization failed");
+                if !compare_docs(&doc, &reserialized_doc) {
+                    panic!(
+                        "Reserialized document is not the same as the original document: {:?} != \
+                         {:?}",
+                        doc, reserialized_doc
+                    );
+                }
             }
         }
     }
