73e26b0f1bf284e95cc1f98d84119e9dadc53b7d
Abraham Egnor <abraham.egnor@mongodb.com>
2025-06-10 15:32:29 -0400

    pick up renamed files

---
+++ fuzz/fuzz_targets/raw_decode_utf8_lossy.rs
@@ -0,0 +1,11 @@
+#![no_main]
+#[macro_use] extern crate libfuzzer_sys;
+extern crate bson;
+use bson::Document;
+
+fuzz_target!(|buf: &[u8]| {
+    if let Ok(doc) = bson::from_slice_utf8_lossy::<Document>(buf) {
+        let mut vec = Vec::with_capacity(buf.len());
+        let _ = doc.to_writer(&mut vec);
+    }
+});
