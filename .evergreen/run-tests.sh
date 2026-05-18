30e8c1359ae29986a6cbaf359f470a50354691d2
Abraham Egnor <abraham.egnor@mongodb.com>
2026-05-18 10:37:03 +0100

    include default feature doctests in evergreen

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -4,7 +4,5 @@ set -o errexit
 
 . ~/.cargo/env
 
-# Test with default features and excluding doctests (some of which require the 'serde' feature)
-RUST_BACKTRACE=1 cargo test --all-targets
-# Test with all features and including doctests
+RUST_BACKTRACE=1 cargo test
 RUST_BACKTRACE=1 cargo test --all-features
